7172c0e2b4f58637571a6e7f79e336109c866651
jjh5887 <wjdrnjsgh159@naver.com>
2021-10-12 10:32:10 +0900

    TT-1 프로젝트 기본 환경 설정

---
+++ src/main/java/me/powerarc/taketogether/TakeTogetherApplication.java
@@ -0,0 +1,13 @@
+package me.powerarc.taketogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeTogetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/powerarc/taketogether/TakeTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package me.powerarc.taketogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
